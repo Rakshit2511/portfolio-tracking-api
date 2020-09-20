deb7c8d1aacc8ba11ac04d497fef285f64b4214a
Rakshit Bhalla <rakshit.bhalla@yahoo.com>
2020-09-20 18:41:57 +0530

    Added basic project from spring.io

---
+++ src/test/java/dev/rakshit/portfoliotrackingapi/PortfolioTrackingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.rakshit.portfoliotrackingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioTrackingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
